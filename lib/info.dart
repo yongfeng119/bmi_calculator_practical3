6f9ddfd1ce25ef7acc6cb8cb94fa1b5c7f3623a8
Taruc <titusyf045936@gmail.com>
2025-07-11 17:55:41 +0800

    Second commit

--- lib/info.dart
+++ lib/info.dart
@@ -8,9 +8,7 @@ class Info extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Info'),
-      ),
+      appBar: AppBar(title: const Text('BMI Info')),
       body: Padding(
         padding: EdgeInsets.all(8.0),
         child: Center(child: Column(children: [
@@ -18,43 +16,46 @@ class Info extends StatelessWidget {
             border: TableBorder.all(color: Colors.orangeAccent),
             children: [
               TableRow(
-                children: const [
-                  TableCell(child: Text('BMI Value',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  ),
-                ],
+                  children: const [
+                    TableCell(child: Text('BMI Value',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    ),
+                    TableCell(child: Text('Classification',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    ),
+                  ]
               ),
               TableRow(
-                children: [
-                  TableCell(
-                      child: Text('Below 18.5',textAlign: TextAlign.center)),
-                  TableCell(
-                      child: Text('Underweight',textAlign: TextAlign.center))
-                ]
+                  children: [
+                    TableCell(
+                        child: Text('Below 18.5',textAlign: TextAlign.center)),
+                    TableCell(
+                        child: Text('Underweight',textAlign: TextAlign.center)),
+                  ]
               ),
               TableRow(
-                children: [
-                  TableCell(
-                      child: Text('18.5 - 24.9',textAlign: TextAlign.center)),
-                  TableCell(
-                      child: Text('Normal',textAlign: TextAlign.center))
-                ]
+                  children: [
+                    TableCell(
+                        child: Text('18.5 - 24.9',textAlign: TextAlign.center)),
+                    TableCell(
+                        child: Text('Normal',textAlign: TextAlign.center)),
+                  ]
               ),
               TableRow(
                   children: [
                     TableCell(
                         child: Text('Above 25',textAlign: TextAlign.center)),
                     TableCell(
-                        child: Text('Overweight',textAlign: TextAlign.center))
+                        child: Text('Overweight',textAlign: TextAlign.center)),
                   ]
               ),
             ],
-          ),
-        ],
-        ),
-        ),
+          )
+        ],),),
       ),
     );
   }
